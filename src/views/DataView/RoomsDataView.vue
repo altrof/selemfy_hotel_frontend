5e71cbf500fb9309b5b9ac55b96ae0ab5a240a81
Aleksandr Trofimov <aleksandr.trofimov@bigbank.ee>
2022-10-24 00:46:13 +0300

    HOT-48 Add two views to BackendTestHealth and Rooms

---
+++ src/views/DataView/RoomsDataView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ResponseInfo from "@/components/ResponseInfo.vue";
+import { useRoomsStore } from "@/stores/rooms";
+import { storeToRefs } from 'pinia';
+
+const { responseData, apiUrl } = storeToRefs(useRoomsStore());
+</script>
+
+<template>
+  <ResponseInfo :response-data="responseData" :api-url="apiUrl" />
+</template>
