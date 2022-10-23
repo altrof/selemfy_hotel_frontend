5eae443982e2082abbd703371ac68ff140de2f2d
Aleksandr Trofimov <aleksandr.trofimov@bigbank.ee>
2022-10-24 00:31:55 +0300

    HOT-48 Add two stores for `backendTestHealth` and `rooms`

---
+++ src/stores/rooms.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import RoomsAPI from "@/services/modules/RoomsAPI";
+
+export const useRoomsStore = defineStore("Rooms", () => {
+    const responseData = ref(null);
+    const apiUrl = ref(null);
+
+    RoomsAPI.getAllRooms()
+        .then(response => {
+            responseData.value = response;
+            apiUrl.value = response.config.baseURL;
+        })
+
+    return {
+        responseData,
+        apiUrl
+    }
+})
