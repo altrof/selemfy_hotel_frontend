37549a40b60d014820c09408b01fe2a940913912
Norman Kuusik <normankuusik@gmail.com>
2022-10-28 13:21:52 +0300

    Well styled beautiful table.

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,15 +1,39 @@
 <script setup>
 
   defineProps({
-    tableData: Object
+    tableData: Array
   })
 </script>
 
 <template>
-    <table id="tableComponent" >
-      <thead v-for="(item, key) in tableData" :key="key">
-          <th>{{key}} </th>
-          <td>{{item}}</td>   
+    <table class="table_component" >
+      <thead class="header" >
+        <tr >
+          <td v-for="(value, key) in tableData[0]" :key="key">{{key}}</td>   
+        </tr>
       </thead>
+      <tbody v-for="(object) in tableData" :key="object">
+        <tr>
+          <td v-for="(element) in object" :key="element">{{element}} </td>
+        </tr>
+      </tbody>
+
     </table>
 </template>
+
+<style scoped>
+  .table_component {
+    background-color: rgba(226, 226, 226, 0.938);
+    text-align: center;
+    border-collapse: collapse;
+
+  }
+
+  .table_component tr {
+        border-bottom: 1px solid black;
+  }
+
+  .header {
+    background-color: rgba(25, 25, 255, 0.527);
+  }
+</style>
--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,13 +1,27 @@
 <script setup>
 import Table from '../components/Table.vue'
 const testObject = 
-    {
+    [{
       "size": 5,
       "floorId": 1,
       "roomNumber": 1,
       "numberOfBeds": 2,
-      "roomType": "REGULAR"
-    }
+      "roomType": "REGULAR",
+    },
+    {
+      "size": 50,
+      "floorId": 1,
+      "roomNumber": 1,
+      "numberOfBeds": 2,
+      "roomType": "DELUXE"
+    },
+        {
+      "size": 50,
+      "floorId": 1,
+      "roomNumber": 1,
+      "numberOfBeds": 2,
+      "roomType": "DELUXE"
+    }]
 </script>
 
 <template>
