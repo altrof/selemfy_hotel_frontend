e4addc2bb4cb1e1439220090271de36e8b3b396c
Norman Kuusik <normankuusik@gmail.com>
2022-10-28 14:04:42 +0300

    Add functionality to view data about people from a table in front end.

---
+++ src/views/DataView/PersonTableView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Table from "@/components/Table.vue";
+import DataMainView from '@/views/DataView/DataMainView.vue'
+import { usePersonstore } from "@/stores/person";
+import { storeToRefs } from 'pinia';
+
+
+
+const { responseData, apiUrl } = storeToRefs(usePersonstore());
+</script>
+
+<template>
+  <DataMainView />
+  <div class="container">
+    <Table :tableData="responseData.data" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding-top: 10%;
+  padding-left: 25%;
+}
+</style>
