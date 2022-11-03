729d7cf965093dc9841d6049565eec48d0577cdf
Aleksandr Trofimov <aleksandr.trofimov@bigbank.ee>
2022-11-03 23:24:33 +0200

    HOT-63 Create new Molecules components: BookingForm, ContentWrapper, ModalWindow, MyProfileNav

---
+++ src/components/_molecules/BookingForm/BookingForm.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+  <div data-testid="booking-form" class="booking-form">
+    BOOKING FORM
+  </div>
+</template>
+
+<style scoped>
+.booking-form {
+  background-color: white;
+  border: 1px solid black;
+}
+
+</style>
