191cfdf472a172bde44e83fdcab6961265b77441
Norman Kuusik <normankuusik@gmail.com>
2022-11-06 17:13:57 +0200

    Improve behaviour of BaseButton.

--- src/components/_atoms/BaseButton/BaseButton.vue
+++ src/components/_atoms/BaseButton/BaseButton.vue
@@ -6,10 +6,10 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="hover:bg-blue-500 text-gray-700 
-                hover:text-white px-5 border-2 border-gray-800 
-                hover:border-transparent rounded
-                mx-0.5">
-        <button @click="$emit('clickHandler')">{{textContent}}</button>
-    </div>
+    <button class="hover:bg-blue-500 text-gray-700 
+            hover:text-white px-5 border-2 border-gray-800 
+            hover:border-transparent rounded
+            mx-0.5" @click="$emit('clickHandler')">
+        {{textContent}}
+    </button>
 </template>
