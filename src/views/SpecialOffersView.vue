e91fc70b3de2ad16e921d16560d37771a36410a1
Norman Kuusik <normankuusik@gmail.com>
2022-11-07 14:05:53 +0200

    Use a loop in SpecialOffersView instead of hardcoded components.

--- src/views/SpecialOffersView.vue
+++ src/views/SpecialOffersView.vue
@@ -3,21 +3,20 @@ import Navbar from '@/components/_organisms/Navbar/Navbar.vue';
 import Product from '@/components/_organisms/Product/Product.vue';
 import ContentWrapper from '@/components/_molecules/ContentWrapper/ContentWrapper.vue'
 
+const products = [
+      {name: 'Parking', image: "parking.webp"}, 
+      {name: 'Champagne', image: "champagne.jpeg"}, 
+      {name: 'Shuttle', image: "shuttle.png"}
+      ]
 const imagesDir = "service-imgs"
-const parkingImage = "parking.webp"
-const champagneImage = "champagne.jpeg"
-const shuttleImage = "shuttle.png"
 </script>
 
 <template>
   <Navbar />
   <ContentWrapper>
     <p>Special Offers</p>
-    <div class="flex flex-wrap place-content-around">
-        <Product product-name="Parking" product-description="Enjoy this wonderful special offer on our behalf" :file-name="parkingImage" :dir-name="imagesDir"/>
-        <Product product-name="Champagne" product-description="Enjoy this wonderful special offer on our behalf" :file-name="champagneImage" :dir-name="imagesDir"/>
-        <Product product-name="Shuttle" product-description="Enjoy this wonderful special offer on our behalf" :file-name="shuttleImage" :dir-name="imagesDir"/>
-        <Product product-name="Product Name" product-description="Enjoy this wonderful special offer on our behalf" :file-name="champagneImage" :dir-name="imagesDir"/>        
+    <div class="flex flex-wrap place-content-around" >
+        <Product v-for="(product) in products" :key="product" :product-name="product.name" product-description="Enjoy this wonderful special offer on our behalf" :file-name="product.image" :dir-name="imagesDir"/>
     </div>
   </ContentWrapper>
 </template>
