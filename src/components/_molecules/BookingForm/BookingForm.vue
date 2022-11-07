b93b841494983e0c033e9c98628941db36863ab7
Anti Natka <anti.natka@ttu.ee>
2022-11-07 14:13:51 +0200

    Played around and added something basic

--- src/components/_molecules/BookingForm/BookingForm.vue
+++ src/components/_molecules/BookingForm/BookingForm.vue
@@ -1,10 +1,49 @@
-<script>
+<script setup>
+import { ref } from 'vue'
+import BaseInput from "@/components/_atoms/BaseInput/BaseInput.vue";
+
+
+const date = ref('Some date')
+const duration = ref(' duration ')
+const persons = ref(' persons ')
+const room = ref(' room ')
 
 </script>
 
 <template>
-  <div data-testid="booking-form" class="booking-form">
-    BOOKING FORM
+  <div>
+    <BaseInput
+        v-model="date"
+        label="date"
+        placeholder="date"
+        autocomplete="on"
+    />
+    <BaseInput
+        v-model="duration"
+        label="Nights"
+        placeholder="Nights"
+        autocomplete="on"
+    />
+    <BaseInput
+        v-model="persons"
+        label="Persons"
+        placeholder="date"
+        autocomplete="on"
+    />
+    <b-dropdown text="Button text via Prop">
+      <b-dropdown-item href="#">An item</b-dropdown-item>
+      <b-dropdown-item href="#">Another item</b-dropdown-item>
+    </b-dropdown>
+    <select
+        v-model="room"
+        label="Room Type"
+        placeholder="Room Type"
+        autocomplete="on"
+    />
+
+
+    <button> {{persons}}</button>
+    <button> {{room}}</button>
   </div>
 </template>
 
