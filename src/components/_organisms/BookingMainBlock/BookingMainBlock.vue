178c3fd335017c957d15e90599a08d6ce3422c03
rovald <rovald@ttu.ee>
2022-11-08 17:43:44 +0200

    Implemented slider but don't know how to fix transition so that it looks good.

--- src/components/_organisms/BookingMainBlock/BookingMainBlock.vue
+++ src/components/_organisms/BookingMainBlock/BookingMainBlock.vue
@@ -1,32 +1,27 @@
-<script>
-import Slider from '@/components/_atoms/Slider/Slider.vue';
-export default {
-  name: "slider-app",
-  components: {
-    Slider
-  }
-};
-</script>
-
 <script setup>
 import BookingForm from '@/components/_molecules/BookingForm/BookingForm.vue';
+const carouselSlides = ["src/assets/img/slider-imgs/hero-1.jpg",
+                        "src/assets/img/slider-imgs/hero-2.jpg",
+                        "src/assets/img/slider-imgs/hero-3.jpg"];
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide} from 'vue3-carousel'
 </script>
 
 <template>
   <div data-testid="main-block" class="main-block">
     <BookingForm class="booking-form" />
+      <div>
+        <carousel :autoplay="5000" :itemsToShow="1" :transition="700" :wrapAround="true" :pauseAutoplayOnHover="false">
+          <slide v-for="slide in carouselSlides" :key="slide">
+            <img :src="slide" alt="" />
+          </slide>
+        </carousel>
+      </div>
     <!-- ToDo: Create own slider (or you can use external library, such as VueSlider)-->
-    <div id="slider-app">
-      <Slider />
-      <!-- <img src="@/assets/img/slider-imgs/hero-1.jpg" /> -->
-      <!-- img src="@/assets/img/slider-imgs/hero-2.jpg" /> -->
-      <!-- <img src="@/assets/img/slider-imgs/hero-3.jpg" /> -->
-    </div>
   </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
 .main-block {
   display: flex;
   justify-content: center;
@@ -41,5 +36,7 @@ import BookingForm from '@/components/_molecules/BookingForm/BookingForm.vue';
   padding: 10px;
   width: 70%;
   justify-content: center;
+  z-index: 1;
 }
+
 </style>
