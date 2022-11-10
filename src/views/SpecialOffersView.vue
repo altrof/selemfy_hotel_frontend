93ab748443a6848ac07667a5927307cff8600a7a
Norman Kuusik <normankuusik@gmail.com>
2022-11-11 01:04:00 +0200

    Resize and crop images.

--- src/views/SpecialOffersView.vue
+++ src/views/SpecialOffersView.vue
@@ -9,12 +9,12 @@ Products are currently hard-coded, but perhaps
 we should request the same information from the database?
 */
 const products = [
-      {name: 'Parking', image: "parking.webp", price: 10}, 
+      {name: 'Parking', image: "parking.jpg", price: 10}, 
       {name: 'Champagne', image: "champagne.jpeg", price: 15}, 
       {name: 'Shuttle', image: "shuttle.png", price: 5},
       {name: 'Massage', image: "massage.jpg", price: 50},
-      {name: 'Restaurant', image: "restaurant.webp", price: 25},
-      {name: 'Wake up call', image: "wake-up-call.webp", price: 5}
+      {name: 'Restaurant', image: "restaurant.jpg", price: 25},
+      {name: 'Wake up call', image: "wake-up-call.jpg", price: 5}
       ]
 const imagesDir = "service-imgs"
 </script>
