e50e1fc20263daf1ae90d53682d201a451c353f8
Norman Kuusik <normankuusik@gmail.com>
2022-11-13 23:01:39 +0200

    Test deployment twice more

--- src/views/MainHotelView.vue
+++ src/views/MainHotelView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import Navbar from "@/components/_organisms/Navbar";
+//import Navbar from "@/components/_organisms/Navbar";
 import LoremIpsum from "@/components/_atoms/LoremIpsum/LoremIpsum.vue";
 //import BookingMainBlock from "@/components/_organisms/BookingMainBlock";
 import ContentWrapper from '@/components/_molecules/ContentWrapper/ContentWrapper.vue'
