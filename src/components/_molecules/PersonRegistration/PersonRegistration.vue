5821cde0dee917c976289b847535a51f76148800
Norman Kuusik <normankuusik@gmail.com>
2022-11-14 21:01:27 +0200

    Modify PersonRegistration component to reference input field values in addPersonToBooking().

--- src/components/_molecules/PersonRegistration/PersonRegistration.vue
+++ src/components/_molecules/PersonRegistration/PersonRegistration.vue
@@ -2,7 +2,6 @@
 import BaseInput from '@/components/_atoms/BaseInput/BaseInput.vue'
 import BaseButton from '@/components/_atoms/BaseButton/BaseButton.vue'
 import { usePersonstore } from "@/stores/person";
-import { storeToRefs } from 'pinia';
 
 const { addPersonToBooking } = usePersonstore();
 
@@ -10,10 +9,34 @@ const { addPersonToBooking } = usePersonstore();
 
 <template>
     <div class="p-10 border-4 border-gray-200 rounded-lg m-2">
-        <BaseInput label="First Name" placeholder="John" autocomplete="on"/>
-        <BaseInput label="Last Name" placeholder="Smith" autocomplete="on"/>
-        <BaseInput label="Identiy Code" placeholder="61107121760" autocomplete="on"/>
-        <BaseInput abel="Date of Birth" type="date" autocomplete="on"/>
-        <BaseButton class="float-right" @click-handler="addPersonToBooking('123','John', 'Smith', 'fawdwa')" textContent="Add"/>
+        
+        <BaseInput 
+            v-model="firstName" 
+            label="First Name" 
+            placeholder="John" 
+            autocomplete="on"/>
+
+        <BaseInput 
+            v-model="lastName" 
+            label="Last Name" 
+            placeholder="Smith" 
+            autocomplete="on"/>
+
+        <BaseInput 
+            v-model="idCode" 
+            label="Identiy Code" 
+            placeholder="61107121760" 
+            autocomplete="on"/>
+        
+        <BaseInput 
+            v-model="dateOfBirth" 
+            label="Date of Birth" 
+            type="date" autocomplete="on"/>
+
+        <BaseButton 
+            class="float-right" 
+            @click-handler=
+                "addPersonToBooking(idCode, firstName, lastName, dateOfBirth)" 
+            textContent="Add"/>
     </div>
 </template>
