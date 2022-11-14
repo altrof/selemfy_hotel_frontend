7a3c2e3a7e1cb13dbb64ae726d073e912a81e41f
Norman Kuusik <normankuusik@gmail.com>
2022-11-14 20:09:02 +0200

    Add all input fields, basic layout and style.

--- src/components/_molecules/PersonRegistration/PersonRegistration.vue
+++ src/components/_molecules/PersonRegistration/PersonRegistration.vue
@@ -1,10 +1,14 @@
 <script setup>
 import BaseInput from '@/components/_atoms/BaseInput/BaseInput.vue'
-
+import BaseButton from '@/components/_atoms/BaseButton/BaseButton.vue'
 </script>
 
 <template>
-    <p>I am registration form.</p>
-    <BaseInput />
-
+    <div class="p-10 border-4 border-gray-200 rounded-lg m-2">
+        <BaseInput label="First Name" placeholder="John" autocomplete="on"/>
+        <BaseInput label="Last Name" placeholder="Smith" autocomplete="on"/>
+        <BaseInput label="Identiy Code" placeholder="61107121760" autocomplete="on"/>
+        <BaseInput abel="Date of Birth" type="date" autocomplete="on"/>
+        <BaseButton class="float-right" textContent="Add"/>
+    </div>
 </template>
