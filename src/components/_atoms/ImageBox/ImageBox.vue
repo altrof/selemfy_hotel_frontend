45a473ffbf1062843f5eea7e93732ae18ca146b4
Norman Kuusik <normankuusik@gmail.com>
2022-11-16 23:08:50 +0200

    Create global function useImage and implement it on ImageBox.

--- src/components/_atoms/ImageBox/ImageBox.vue
+++ src/components/_atoms/ImageBox/ImageBox.vue
@@ -6,18 +6,14 @@ const props = defineProps({
     name: String
 })
 
-function getImageUrl(fileName, dirName) {
-  return new URL(`/src/assets/img/${dirName}/${fileName}`, import.meta.url)
-
-}
 </script>
 
 <template>
     <div>
         <h1>{{name}}</h1>
-        <img
-           class="my-3 rounded-lg border-2 border-gray-600"
-           :src="getImageUrl(fileName, dirName)"
+        <img 
+           class="my-3 rounded-lg border-2 border-gray-600" 
+           :src="$image(fileName, dirName)" 
            :alt="name">
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router/'
+import router from './router'
 
+
+
+const useImage = ((fileName, dirName) => {
+    return new URL(`/src/assets/img/${dirName}/${fileName}`, import.meta.url)
+  
+  })
 
 const app = createApp(App)
+
+app.config.globalProperties.$image = useImage;
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
