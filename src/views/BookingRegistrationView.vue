5a8d6c83769a235f8ebfec912573fd2cdc1c041b
Norman Kuusik <normankuusik@gmail.com>
2022-11-30 00:06:14 +0200

    Generate PersonRegistration components dynamically based on the size of guests.

--- src/views/BookingRegistrationView.vue
+++ src/views/BookingRegistrationView.vue
@@ -4,17 +4,25 @@ import ContentWrapper from "@/components/_molecules/ContentWrapper/ContentWrappe
 import PersonRegistration from "@/components/_molecules/PersonRegistration/PersonRegistration.vue";
 import RoomSelection from "@/components/_organisms/RoomSelection/RoomSelection.vue";
 import BaseButton from "@/components/_atoms/BaseButton/BaseButton.vue";
-
 import { useBookingStore } from "@/stores/booking.js";
-const { isRoomChosen } = useBookingStore();
+import { onMounted } from "vue";
 
+const { isRoomChosen, amountAdults, amountChildren } = useBookingStore();
+
+onMounted(() => {
+    console.log(isRoomChosen)
+    console.log(amountAdults)
+    console.log(amountChildren)
+});
 </script> 
 
 <template>
   <Navbar />
   <ContentWrapper>
     <p>Registration</p>
-    <PersonRegistration />
+    <div v-for="n in amountAdults + amountChildren">
+        <PersonRegistration />
+    </div>
   </ContentWrapper>
 </template>
 
